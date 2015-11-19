48042701b744a0b954e63f477d85b9528381df4a
Tim Neumann <mail@timnn.me>
2015-11-19 13:41:53 +0100

    add MultiMergeIter

--- src/util.rs
+++ src/util.rs
@@ -3,8 +3,11 @@ use libc;
 use scoped_threadpool::Pool;
 
 use std::cmp::Ordering;
+use std::marker::PhantomData;
 use std::sync::PoisonError;
 use std::sync::{Mutex, MutexGuard};
+
+use binary_heap_by::{BinaryHeapBy, CmpFn};
 
 pub fn stdin_isatty() -> bool {
     unsafe { libc::isatty(0) != 0 }
@@ -60,5 +63,49 @@ impl OrderingExt for Ordering {
             Ordering::Equal => f(),
             _ => self,
         }
+    }
+}
+
+pub struct MultiMergeIter<'a, I: 'a + Iterator, F: CmpFn<I::Item>> {
+    iters: BinaryHeapBy<(I::Item, &'a mut I), Tuple0Cmp<I::Item, F>>,
+}
+
+struct Tuple0Cmp<T, F: CmpFn<T>>(PhantomData<(T, F)>);
+
+impl<T, S, F: CmpFn<T>> CmpFn<(T, S)> for Tuple0Cmp<T, F> {
+    fn cmp(a: &(T, S), b: &(T, S)) -> Ordering {
+        F::cmp(&a.0, &b.0)
+    }
+}
+
+impl<'a, I: 'a + Iterator, F: CmpFn<I::Item>> MultiMergeIter<'a, I, F> {
+    pub fn new(iters: &'a mut [I]) -> MultiMergeIter<'a, I, F> {
+        let mut its = BinaryHeapBy::with_capacity_by(iters.len());
+
+        for iter in iters.iter_mut() {
+            if let Some(el) = iter.next() {
+                its.push((el, iter))
+            }
+        }
+
+        MultiMergeIter {
+            iters: its,
+        }
+    }
+}
+
+impl<'a, I: 'a + Iterator, F: CmpFn<I::Item>> Iterator for MultiMergeIter<'a, I, F> {
+    type Item = I::Item;
+
+    fn next(&mut self) -> Option<I::Item> {
+        self.iters.pop().map(|t| {
+            let this = t.0;
+
+            if let Some(next) = t.1.next() {
+                self.iters.push((next, t.1))
+            }
+
+            this
+        })
     }
 }
